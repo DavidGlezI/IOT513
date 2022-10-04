20429115b6a8ebd28daf309b490481e1f91c22dd
DavidGlezI <A01633817@itesm.mx>
2022-10-04 13:37:39 +0000

    Practica 1

---
+++ exec.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+
+    printf("Prueba\n");
+    int pid = fork();
+
+
+    if(pid == 0){
+        printf("Soy el proceso hijo y me voy a convertir en ls\n");
+        execl("/workspace/IOT513/hola", "holamundo", "/etc", NULL);
+        printf("Esto no debe ejecutarse");        
+    }else{
+        printf("Soy el proceso padre\n");
+    }
+
+    return 0;
+}
